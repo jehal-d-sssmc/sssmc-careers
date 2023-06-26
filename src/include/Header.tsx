771a9e0406bae1d706caa6c0b2a900e20d686b98
jehal-d-sssmc <106730131+jehal-d-sssmc@users.noreply.github.com>
2023-06-26 16:08:04 +0530

    1 file modified

--- src/include/Header.tsx
+++ src/include/Header.tsx
@@ -4,18 +4,20 @@ class Header extends React.Component {
   render() {
     return (
       <>
-        <header className="pb-3 mb-4 border-bottom">
-          <a
-            href="/"
-            className="d-flex align-items-center text-dark text-decoration-none"
-          >
-            <img
-              src="https://www.sssmediacentre.org/755f75139b00a808ae072efc6cdd7ae8.png"
-              alt=""
-            />
-            <span className="fs-4">Sri Sathya Sai Media Centre</span>
-          </a>
-        </header>
+        <div className="container">
+          <header className="pb-3 mb-4 border-bottom">
+            <a
+              href="/"
+              className="d-flex align-items-center text-dark text-decoration-none"
+            >
+              <img
+                src="https://www.sssmediacentre.org/755f75139b00a808ae072efc6cdd7ae8.png"
+                alt=""
+              />
+              <span className="fs-4">Sri Sathya Sai Media Centre</span>
+            </a>
+          </header>
+        </div>
       </>
     );
   }
